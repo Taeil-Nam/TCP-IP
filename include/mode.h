b04344c8afc66e0dbdb1f5d791df4d1db9caf73c
Taeil Nam <nam0314@gmail.com>
2025-04-07 16:52:48 +0900

    Feat: Add mode selection menu

---
+++ include/mode.h
@@ -0,0 +1,10 @@
+#ifndef MODE_H
+#define MODE_H
+
+enum tcpip_mode {
+	TCPIP_MODE_EXIT,
+	TCPIP_MODE_RECV,
+	TCPIP_MODE_SEND,
+};
+
+#endif
---
+++ src/recv_mode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "recv_mode.h"
+
+void start_recv_mode(){
+	printf("Running in RECEIVER mode\n");
+}
