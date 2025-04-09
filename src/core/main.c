250a447110bf2372cdaba5d57ded521ca518a600
Taeil Nam <nam0314@gmail.com>
2025-04-09 14:31:27 +0900

    epoll: Added epoll instance
    Create epoll instance and added raw socket into epoll as read event.
    If packet received in raw socket, epoll notice that event.
    Thus, we can processing that received packet.
    
    I used epoll for handling multiple socket I/O later.
    
    Signed-off-by: Taeil Nam <nam0314@gmail.com>

--- src/core/main.c
+++ src/core/main.c
@@ -2,15 +2,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <linux/if_packet.h>
-#include <net/ethernet.h>
-#include <linux/if_ether.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <net/ethernet.h>
+
+#define MAX_EVENTS 100
 
 static int raw_sock = -1;
+static int epoll_fd = -1;
 
 static void init(void);
 static void run(void);
@@ -24,30 +29,64 @@ int main()
 
 static void init(void)
 {
+	int flag;
+	struct epoll_event ev;
+
 	/* Create raw socket */
 	raw_sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+
 	if (raw_sock == -1) {
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
 
 	/* Set socket as non-blocking */
-	int flag = fcntl(raw_sock, F_GETFL, 0);
+	flag = fcntl(raw_sock, F_GETFL, 0);
 
 	if (flag == -1) {
 		perror("fcntl");
 		exit(EXIT_FAILURE);
 	}
+
 	if (fcntl(raw_sock, F_SETFL, flag | O_NONBLOCK) == -1) {
 		perror("fcntl");
 		exit(EXIT_FAILURE);
 	}
 
 	/* Create epoll */
-	/* Add stdin recv event into epoll*/
-	/* Add raw socket recv event into epoll */
+	epoll_fd = epoll_create1(0);
+
+	if (epoll_fd == -1) {
+		perror("epoll_create1");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Add raw socket into epoll */
+	ev.events = EPOLLIN;
+	ev.data.fd = raw_sock;
+
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, raw_sock, &ev) == -1) {
+		perror("epoll_ctl");
+		exit(EXIT_FAILURE);
+	}
 }
 
 static void run(void)
 {
+	/* Check received packet and processing it */
+	while (true) {
+		struct epoll_event events[MAX_EVENTS];
+		int ready_cnt = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+		int i;
+
+		if (ready_cnt == -1) {
+			perror("epoll_wait");
+			exit(EXIT_FAILURE);
+		}
+
+		for (i = 0; i < ready_cnt; i++) {
+			if (events[i].data.fd == raw_sock)
+				printf("Packet received.\n");
+		}
+	}
 }
